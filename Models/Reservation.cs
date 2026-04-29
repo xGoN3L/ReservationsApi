63257332408c5b2403089e86e45961cbf4c53fbc
xgon3l <s29784@pjwstk.edu.pl>
2026-04-29 21:45:04 +0200

    feat: add Reservation model

---
+++ Models/Reservation.cs
@@ -0,0 +1,14 @@
+﻿namespace ReservationsApi.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int RoomId { get; set; }
+        public string OrganizerName { get; set; } = string.Empty;
+        public string Topic { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public ReservationStatus Status { get; set; }
+    }
+}
