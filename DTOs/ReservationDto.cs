951bfd5eb9e06ff4c779f06d898a7637e04fce38
xgon3l <s29784@pjwstk.edu.pl>
2026-04-29 21:55:54 +0200

    feat: add ReservationDto class

---
+++ DTOs/ReservationDto.cs
@@ -0,0 +1,15 @@
+﻿using ReservationsApi.Models;
+
+namespace ReservationsApi.DTOs
+{
+    public class ReservationDto
+    {
+        public int RoomId { get; set; }
+        public string OrganizerName { get; set; } = string.Empty;
+        public string Topic { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public ReservationStatus Status { get; set; }
+    }
+}
