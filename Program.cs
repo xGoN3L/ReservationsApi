e03a545c5f1293bd18329dc4d012d4a201c76b41
xgon3l <s29784@pjwstk.edu.pl>
2026-05-04 17:10:39 +0200

    feat: add JsonOptions with JsonStringEnumConverter to Services

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
