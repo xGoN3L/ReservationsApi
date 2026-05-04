f442124aec085e2fd8b26a3a69833f57e60ff808
xgon3l <s29784@pjwstk.edu.pl>
2026-05-04 14:34:47 +0200

    feat: add Room data annotations

--- Models/Room.cs
+++ Models/Room.cs
@@ -1,11 +1,16 @@
-﻿namespace ReservationsApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReservationsApi.Models
 {
     public class Room
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
+        [Required]
         public string BuildingCode { get; set; } = string.Empty;
         public int Floor { get; set; }
+        [Range(1, int.MaxValue)]
         public int Capacity { get; set; }
         public bool HasProjector { get; set; }
         public bool IsActive { get; set; }
