e3237f37935e2c57c0193fd2177262274fa20e08
xgon3l <s29784@pjwstk.edu.pl>
2026-05-04 14:34:39 +0200

    feat: add Reservation data annotations

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -1,10 +1,14 @@
-﻿namespace ReservationsApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReservationsApi.Models
 {
     public class Reservation
     {
         public int Id { get; set; }
         public int RoomId { get; set; }
+        [Required]
         public string OrganizerName { get; set; } = string.Empty;
+        [Required]
         public string Topic { get; set; } = string.Empty;
         public DateTime Date { get; set; }
         public TimeSpan StartTime { get; set; }
